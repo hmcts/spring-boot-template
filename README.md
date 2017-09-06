c3c612e6c6436be593482f3582dac65bef842d41
leszekg <gonczar@gmail.com>
2017-09-06 11:42:15 +0100

    RPE-31: Create an initial version of Spring Boot application template

---
+++ README.md
@@ -0,0 +1,10 @@
+# Spring Boot application template
+
+## Purpose
+
+The purpose of this template is to speed up the creation of new Spring applications within HMCTS
+and help keep the same standards across multpile teams. If you need to create a new app, you can
+simply use this one as a starting point and build on top of it.
+
+
+TODO: add details
---
+++ src/main/java/uk/gov/hmcts/reform/demo/Application.java
@@ -0,0 +1,13 @@
+package uk.gov.hmcts.reform.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// TODO: change the name of this class to one matching your project, e.g. DraftStoreApplication
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
