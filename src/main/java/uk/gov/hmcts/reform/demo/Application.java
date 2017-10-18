228a573cab56af19fee6f72f7ec15a18d7d67256
Tim Jacomb <tim.jacomb@hmcts.net>
2017-10-18 09:16:52 +0100

    Upgrade checkstyle
    I upgraded claimstore checkstyle to remove the hack that we used to fail on
    warnings as there is now a property for it, also updated rules to
    closely match what we have in the claimstore, happy to undo them if you
    don't see any value.
    
    Rules such as no unused imports and hide utility class constructors have
    been added.

--- src/main/java/uk/gov/hmcts/reform/demo/Application.java
+++ src/main/java/uk/gov/hmcts/reform/demo/Application.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
 @SpringBootApplication
 @EnableCircuitBreaker
 @EnableHystrixDashboard
+@SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 public class Application {
 
     public static void main(String[] args) {
