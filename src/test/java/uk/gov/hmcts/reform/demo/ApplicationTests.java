0abd33e38b562860e05cd8d932dfb405599ef7a8
Rafal Kondratowicz <rkondratowicz@users.noreply.github.com>
2017-11-24 15:27:23 +0000

    Update pmd ruleset (#15)

--- src/test/java/uk/gov/hmcts/reform/demo/ApplicationTests.java
+++ src/test/java/uk/gov/hmcts/reform/demo/ApplicationTests.java
@@ -5,12 +5,15 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTests {
 
     @Test
-    public void contextLoads() {
+    public void sample_test() {
+        assertThat(2).isGreaterThan(1);
     }
 
 }
