6e7f220a776e017613abd76093a163d70f54eac3
Donatas Martinkus <donatas.martinkus@hmcts.net>
2018-03-19 16:08:19 +0000

    Add docblock for root endpoint

--- src/main/java/uk/gov/hmcts/reform/demo/controllers/HelloWorldEndpoint.java
+++ src/main/java/uk/gov/hmcts/reform/demo/controllers/HelloWorldEndpoint.java
@@ -6,9 +6,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 import static org.springframework.http.ResponseEntity.ok;
 
+/**
+ * Default endpoints per application.
+ */
 @RestController
 public class HelloWorldEndpoint {
 
+    /**
+     * Root endpoint.
+     *
+     * <p>Azure application service has a hidden feature of making requests to root endpoint when
+     * "Always On" is turned on.
+     * This is the endpoint to deal with that and therefore silence the unnecessary 404s as a response code.
+     *
+     * @return Welcome message from the service.
+     */
     @GetMapping
     public ResponseEntity<String> helloWorld() {
         return ok("Welcome to spring-boot-template");
