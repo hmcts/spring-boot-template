3da97060d64191ded0b30519f426fe188c5e1b9d
Tim Jacomb <t.jacomb@kainos.com>
2018-10-04 07:27:19 +0100

    Remove resource group definition (#56)
    Its defined in module web app, having it in main.tf means tagging doesn't get applied

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,11 +1,5 @@
 provider "azurerm" {}
 
-# Make sure the resource group exists
-resource "azurerm_resource_group" "rg" {
-  name     = "${var.product}-${var.component}-${var.env}"
-  location = "${var.location_app}"
-}
-
 locals {
   ase_name               = "${data.terraform_remote_state.core_apps_compute.ase_name[0]}"
 }
