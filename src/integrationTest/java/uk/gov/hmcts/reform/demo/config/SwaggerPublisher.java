a7b930fc29f25faa7ff0e28239185695601dd44f
Donatas Martinkus <donatas.martinkus@hmcts.net>
2018-11-09 15:55:09 +0000

    Introduce documentation publisher

---
+++ src/integrationTest/java/uk/gov/hmcts/reform/demo/config/SwaggerPublisher.java
@@ -0,0 +1,41 @@
+package uk.gov.hmcts.reform.demo.config;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.io.FileOutputStream;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Built-in feature which saves service's swagger specs in temporary directory.
+ * Each travis run on master should automatically save and upload (if updated) documentation.
+ */
+@SpringJUnitWebConfig
+@SpringBootTest
+@AutoConfigureMockMvc
+public class SwaggerPublisher {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @DisplayName("Generate swagger documentation")
+    @Test
+    public void generateDocs() throws Exception {
+        byte[] specs = mvc.perform(get("/v2/api-docs"))
+            .andExpect(status().isOk())
+            .andReturn()
+            .getResponse()
+            .getContentAsByteArray();
+
+        FileOutputStream outputStream = new FileOutputStream("/tmp/swagger-specs.json");
+        outputStream.write(specs);
+        outputStream.close();
+    }
+}
