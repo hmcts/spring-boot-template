007ad8f371ea4b518d7d49a58563aaff763c922c
David Gimelle <davidgimelle1@hmcts.net>
2020-05-11 14:51:45 +0100

    update default unit test

--- src/test/java/uk/gov/hmcts/reform/rsecheck/DemoUnitTest.java
+++ src/test/java/uk/gov/hmcts/reform/rsecheck/DemoUnitTest.java
@@ -1,12 +1,13 @@
 package uk.gov.hmcts.reform.rsecheck;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class DemoUnitTest {
 
     @Test
-    public void ExampleOfTest() {
-        assertTrue(System.currentTimeMillis()>0);
+    public void exampleOfTest() {
+        assertTrue(System.currentTimeMillis() > 0, "Example of Unit Test");
     }
 }
