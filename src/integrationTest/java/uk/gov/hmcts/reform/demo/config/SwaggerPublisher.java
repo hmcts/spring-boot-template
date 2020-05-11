8c96768d571b3ba3764ea55430b2cdec51df49c1
David Gimelle <davidgimelle1@hmcts.net>
2020-05-11 14:36:57 +0100

    Add a default Unit Test

--- src/integrationTest/java/uk/gov/hmcts/reform/demo/config/SwaggerPublisher.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/demo/config/SwaggerPublisher.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.demo.config;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
---
+++ src/test/java/uk/gov/hmcts/reform/rsecheck/DemoUnitTest.java
@@ -0,0 +1,12 @@
+package uk.gov.hmcts.reform.rsecheck;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DemoUnitTest {
+
+    @Test
+    public void ExampleOfTest() {
+        assertTrue(System.currentTimeMillis()>0);
+    }
+}
