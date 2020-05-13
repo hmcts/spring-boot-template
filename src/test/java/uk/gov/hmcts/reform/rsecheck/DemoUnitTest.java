c4dace0d435c102043bb12def1c10a8b3235708c
David Gimelle <davidgimelle@gmail.com>
2020-05-13 11:20:47 +0100

    Fix Project generation (#180)
    * remove web config error
    
    * Fix integration test
    
    * Add a default Unit Test
    
    * update default unit test
    
    * Fix Swagger Test
    
    Co-authored-by: David Gimelle <davidgimelle1@hmcts.net>

---
+++ src/test/java/uk/gov/hmcts/reform/rsecheck/DemoUnitTest.java
@@ -0,0 +1,13 @@
+package uk.gov.hmcts.reform.rsecheck;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DemoUnitTest {
+
+    @Test
+    public void exampleOfTest() {
+        assertTrue(System.currentTimeMillis() > 0, "Example of Unit Test");
+    }
+}
