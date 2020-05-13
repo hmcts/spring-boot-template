c6292d182beef462d00265f2f63e9a158f7feae3
David Gimelle <davidgimelle1@hmcts.net>
2020-05-13 16:22:37 +0100

    Update Charts Folder

--- bin/init.sh
+++ bin/init.sh
@@ -20,13 +20,19 @@ pushd $(dirname "$0")/.. > /dev/null
 slug="$product_name-$component_name"
 
 declare -a files_with_port=(.env Dockerfile README.md src/main/resources/application.yaml charts/rpe-spring-boot-template/values.yaml)
-declare -a files_with_slug=(build.gradle docker-compose.yml Dockerfile README.md ./infrastructure/main.tf ./src/main/java/uk/gov/hmcts/reform/demo/controllers/RootController.java)
+declare -a files_with_slug=(build.gradle docker-compose.yml Dockerfile README.md ./infrastructure/main.tf ./src/main/java/uk/gov/hmcts/reform/demo/controllers/RootController.java charts/rpe-spring-boot-template/Chart.yaml)
 
 # Replace image repo
-for i in "values.yaml"
+for i in "charts/rpe-spring-boot-template/values.yaml"
 do
   perl -i -pe "s/rpe/$product_name/g" ${i}
   perl -i -pe "s/spring-boot-template/$component_name/g" ${i}
+done
+
+#update maintainer name
+for i in "charts/rpe-spring-boot-template/Chart.yaml"
+do
+  perl -i -pe "s/rpe/$product_name/g" ${i}
 done
 
 # Replace port number
@@ -44,6 +50,9 @@ done
 # Replace demo package in all files under ./src
 find ./src -type f -print0 | xargs -0 perl -i -pe "s/reform.demo/reform.$package/g"
 perl -i -pe "s/reform.demo/reform.$package/g" build.gradle
+
+# Rename charts directory
+git mv charts/rpe-spring-boot-template charts/${slug}
 
 # Rename directory to provided package name
 git mv src/integrationTest/java/uk/gov/hmcts/reform/demo/ src/integrationTest/java/uk/gov/hmcts/reform/${package}
