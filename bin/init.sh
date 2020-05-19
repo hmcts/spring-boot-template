5e9f5b4e406897ec4ebdc1cd73bc9c53478385b8
David Gimelle <davidgimelle1@hmcts.net>
2020-05-19 15:22:18 +0100

    Generate CNP file

--- bin/init.sh
+++ bin/init.sh
@@ -13,6 +13,7 @@ read -p "Port number for new app: " port
 read -p "Replace \`demo\` package name with: " package
 read -p "Repo product: (It's first part of the git repo name. Often a team name) " product_name
 read -p "Repo component: (It's second part of git repo name. Application name) " component_name
+read -p "Slack Channel for build notification " slack_channel
 
 
 pushd $(dirname "$0")/.. > /dev/null
@@ -21,6 +22,15 @@ slug="$product_name-$component_name"
 
 declare -a files_with_port=(.env Dockerfile README.md src/main/resources/application.yaml charts/rpe-spring-boot-template/values.yaml)
 declare -a files_with_slug=(build.gradle docker-compose.yml Dockerfile README.md ./infrastructure/main.tf ./src/main/java/uk/gov/hmcts/reform/demo/controllers/RootController.java charts/rpe-spring-boot-template/Chart.yaml)
+
+# Replace in CNP file
+for i in "Jenkinsfile_template"
+do
+  perl -i -pe "s/spring-boot-template/$slug/g" ${i}
+  perl -i -pe "s/rpe-spring-demo-builds/$slack_channel/g" ${i}
+  perl -i -pe "s/rpe/$product_name/g" ${i}
+  perl -i -pe "s/demo/$component_name/g" ${i}
+done
 
 # Replace image repo
 for i in "charts/rpe-spring-boot-template/values.yaml"
@@ -57,6 +67,9 @@ git mv charts/rpe-spring-boot-template charts/${slug}
 # Rename directory to provided package name
 git mv src/integrationTest/java/uk/gov/hmcts/reform/demo/ src/integrationTest/java/uk/gov/hmcts/reform/${package}
 git mv src/main/java/uk/gov/hmcts/reform/demo/ src/main/java/uk/gov/hmcts/reform/${package}
+
+# Rename CNP file
+git mv Jenkinsfile_template Jenkinsfile_CNP
 
 declare -a headers_to_delete=("Purpose" "What's inside" "Plugins" "Setup" "Hystrix")
 
