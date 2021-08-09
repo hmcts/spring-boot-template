803da9aafc4cb4fc6b1fc9cdf35fc7c2f146b0c6
Sam Parker <samwparker96@gmail.com>
2021-08-09 10:56:18 +0100

    Removed hysterix and tomcat

--- src/main/java/uk/gov/hmcts/reform/demo/Application.java
+++ src/main/java/uk/gov/hmcts/reform/demo/Application.java
@@ -2,10 +2,8 @@ package uk.gov.hmcts.reform.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @SpringBootApplication
-@EnableCircuitBreaker
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 public class Application {
 
