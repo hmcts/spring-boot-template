560cf1d6f3c7522cc570f2c77df1525aab4dfb04
chris-pateman <86669094+chris-pateman@users.noreply.github.com>
2021-09-27 16:25:48 +0100

    Add generic smoke tests (#317)
    Co-authored-by: Tim Jacomb <21194782+timja@users.noreply.github.com>
    Co-authored-by: Christopher Pateman <christopher.pateman@version1.com>

---
+++ src/smokeTest/java/uk/gov/hmcts/reform/demo/smoketests/SmokeTestConfiguration.java
@@ -0,0 +1,10 @@
+package uk.gov.hmcts.reform.demo.smoketests;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+
+@ComponentScan("uk.gov.hmcts.reform.demo.smoketests")
+@PropertySource("application.properties")
+public class SmokeTestConfiguration {
+  //TODO: implement smoke tests here
+}
