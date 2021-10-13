8d7ace2154364bcb4476f23c767175a40cff7e87
Tim Jacomb <21194782+timja@users.noreply.github.com>
2021-10-13 16:51:10 +0100

    Update to latest gradle (#318)

--- src/integrationTest/java/uk/gov/hmcts/reform/demo/config/SwaggerPublisherTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/demo/config/SwaggerPublisherTest.java
@@ -17,7 +17,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 /**
  * Built-in feature which saves service's swagger specs in temporary directory.
- * Each travis run on master should automatically save and upload (if updated) documentation.
+ * Each CI run on master should automatically save and upload (if updated) documentation.
  */
 @WebMvcTest
 @ContextConfiguration(classes = SwaggerConfiguration.class)
