332b1fcbc333ffa7f529db043122085a22a58f72
Tim Jacomb <tim.jacomb@hmcts.net>
2022-03-07 16:47:54 +0000

    Update java logging, fix dependency vulnerabilities

--- src/integrationTest/java/uk/gov/hmcts/reform/demo/config/SwaggerPublisherTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/demo/config/SwaggerPublisherTest.java
@@ -2,10 +2,11 @@ package uk.gov.hmcts.reform.demo.config;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.io.OutputStream;
@@ -19,8 +20,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * Built-in feature which saves service's swagger specs in temporary directory.
  * Each CI run on master should automatically save and upload (if updated) documentation.
  */
-@WebMvcTest
-@ContextConfiguration(classes = OpenAPIConfiguration.class)
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 class SwaggerPublisherTest {
 
