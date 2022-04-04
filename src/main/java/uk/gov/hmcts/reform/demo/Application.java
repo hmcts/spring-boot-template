34cde279ab07a41e48eef48fd61fa6c2ab005f2f
Enda Kelly <enda.kelly@hmcts.net>
2022-04-04 08:15:12 +0100

    testing if java 11 works

--- src/main/java/uk/gov/hmcts/reform/demo/Application.java
+++ src/main/java/uk/gov/hmcts/reform/demo/Application.java
@@ -3,11 +3,24 @@ package uk.gov.hmcts.reform.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 @SpringBootApplication
 @SuppressWarnings("HideUtilityClassConstructor") // Spring needs a constructor, its not a utility class
 public class Application {
 
     public static void main(final String[] args) {
+
+        Logger logger
+            = Logger.getLogger(
+                Application.class.getName());
+
         SpringApplication.run(Application.class, args);
+        {
+            String str1 = """
+                Str1 text """;
+            logger.log(Level.INFO, str1);
+        }
     }
 }
